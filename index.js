b311891351a07d04817d54fa26b47412177cdd62
Plan9 <Plan9@46d4d31e-51b8-a24d-967e-622a56c76c62>
2016-10-01 21:52:48 +0000

    キャンバス上の画像操作関連機能を"imaging.js"に移動した。
    git-svn-id: svn://localhost/svn_html5_experiment/branches/PaintBBS_NEO_Next_Generation@13 46d4d31e-51b8-a24d-967e-622a56c76c62

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
-console.log("index.js starts.")
+console.log("index.js starts.");
 
 // イベントハンドラ登録
 window.onload = init;
 
 /// ページのロード完了時に呼ばれる。
-function init() {
-	console.log("init() called.")
+function init()
+{
+	console.log("init() called.");
 
   let layers = [
     document.getElementById("canvas_0"),
@@ -16,15 +17,10 @@ function init() {
   ];
 
 	let m_tool_palette = document.getElementById("tool_pallete");
-
 	let m_joint_canvas = document.getElementById("joint_canvas");
 
 	// キャンバスを白色でfill
-	{
-		let ctx = layers[0].getContext('2d');
-		ctx.fillStyle = "rgba(255, 255, 255, 255)";
-		ctx.fillRect(0, 0, layers[0].width, layers[0].height);
-	}
+	erase_canvas(layers);
 
 	// ツールパレットを背景色でfill
 	{
@@ -37,74 +33,7 @@ function init() {
   sample01(layers);
 
 	// 画像合成
-	{
-		const n = layers.length;
-
-		// 描画先準備
-		const width = layers[0].width;
-		const height = layers[0].height;
-		m_joint_canvas.setAttribute('width', width);
-		m_joint_canvas.setAttribute('height', height);
-		assert(m_joint_canvas.width == width && m_joint_canvas.height == height);
-
-		// 合成対象データ取得
-		let imageDataList = [];
-		for (let i = 0; i < n; ++i) {
-			let ctx = layers[i].getContext('2d');
-			let imgd = ctx.getImageData(0, 0, width, height);
-			assert(imgd.width == width && imgd.height == height);
-			imageDataList[i] = imgd;
-			console.log("imageDataList[" + i + "].data.length=" + imageDataList[i].data.length);
-		}
-
-		// 合成
-		let dst_imgDat = imageDataList[0];	// Ａｌｉａｓ (参照コピーだが問題無い。)
-		for (let py = 0; py < height; ++py) {
-			let head = py * 4 * width;
-			for (let px = 0; px < width; ++px) {
-				let base = head + px * 4;
-				for (let i = 1; i < n; ++i) {		// 奥のレイヤーからα合成していく。
-					// レイヤー[0..i-1]の合成結果(確定済)
-					let R1 = dst_imgDat.data[base + 0];
-					let G1 = dst_imgDat.data[base + 1];
-					let B1 = dst_imgDat.data[base + 2];
-					assert(dst_imgDat.data[base + 3] == 255);
-
-					// レイヤー[i]の画素値
-					let R2 = imageDataList[i].data[base + 0];
-					let G2 = imageDataList[i].data[base + 1];
-					let B2 = imageDataList[i].data[base + 2];
-					let A2 = imageDataList[i].data[base + 3];
-					assert(0 <= A2 && A2 <= 255);
-					// {	// UTEST	画素値確認
-					// 	let A1 = dst_imgDat.data[base + 3];
-					// 	if (px == 150 && py == 150) {
-					// 		console.log("R1=" + R1);
-					// 		console.log("G1=" + G1);
-					// 		console.log("B1=" + B1);
-					// 		console.log("A1=" + A1);
-					// 		console.log("R2=" + R2);
-					// 		console.log("G2=" + G2);
-					// 		console.log("B2=" + B2);
-					// 		console.log("A2=" + A2);
-					// 	}
-					// }
-
-					// 合成
-					let a = 255 - A2;
-					let b = A2;
-					dst_imgDat.data[base + 0] = Math.floor(((R1 * a) + (R2 * b)) / 255.0);
-					dst_imgDat.data[base + 1] = Math.floor(((G1 * a) + (G2 * b)) / 255.0);
-					dst_imgDat.data[base + 2] = Math.floor(((B1 * a) + (B2 * b)) / 255.0);
-					dst_imgDat.data[base + 3] = 255;
-				}
-			}
-		}
-
-		// キャンバスに描画
-		let dst_ctx = m_joint_canvas.getContext('2d');
-		dst_ctx.putImageData(dst_imgDat, 0, 0);
-	}
+	get_joint_image(layers, m_joint_canvas);
 }
 
 //
