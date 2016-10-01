6cb37a7bd921783759206d2d1ac0ef4973700fbb
Plan9 <Plan9@46d4d31e-51b8-a24d-967e-622a56c76c62>
2016-10-01 21:51:20 +0000

    （Rev.9訂正コミット3）（ファイル追加忘れ修正）
    git-svn-id: svn://localhost/svn_html5_experiment/branches/PaintBBS_NEO_Next_Generation@12 46d4d31e-51b8-a24d-967e-622a56c76c62

---
+++ dbg-util.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+//
+//	Debug util
+//
+
+/// イベントをダンプする。
+function dump_event(func, e) {
+	console.log("<" + func + "()>");
+	if (true) {
+		console.log("sender: " + e.sender);
+		console.log("client: (" + e.client.x + ", " + e.client.y + ")");
+		console.log("parent: (" + e.parent.x + ", " + e.parent.y + ")");
+		console.log(" local: (" + e.local.x  + ", " + e.local.y  + ")");
+	} else {
+		console.dir(e);		// お手軽だが表示内容を見るにはコンソールにて手動で展開せねばならない。
+	}
+}
+
+/// Assert関数。下記より拝借。
+/// http://stackoverflow.com/questions/15313418/javascript-assert
+function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
