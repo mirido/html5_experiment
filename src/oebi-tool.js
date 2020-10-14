3a634a4ae8da5a3b7af715bf0caf20e278f98895
mirido <ic342117xwir@yahoo.co.jp>
2020-10-14 09:24:15 +0900

    Tidy import statements

--- src/oebi-tool.js
+++ src/oebi-tool.js
@@ -2,168 +2,39 @@
 // All rights reserved.
 
 import {
-  utest_pre_rendering,
-  utest_ImagePatch,
-  utesst_ColorConversion,
-  utest_canvas_2d_context,
-  utest_get_mask_image,
-  utest_half_tone
-} from './_doc/utest.js';
-import {
-  dump_event,
   assert,
   dbgv
 } from './dbg-util';
 import {
-  get_guide_image,
-  get_mirror_image,
-  get_vert_flip_image,
-  blend_image,
-  fill_image,
-  make_opaque,
-  erase_single_layer,
-  putImageDataEx,
-  erase_canvas,
-  copy_layer,
-  get_destinaton_out_image,
-  get_mask_image,
-  fix_image_w_mask,
-  get_joint_image
-} from './imaging.js';
-import {
-  jsPoint,
-  jsRect,
-  JsPoint,
-  JsRect,
-  decode_rect,
-  encode_to_rect,
-  encode_to_rect_in_place,
-  clip_coords,
-  clip_rect_in_place,
-  clip_rect,
-  get_outbounds,
-  get_common_rect,
-  get_chv_dist,
-  get_mht_dist,
-  rect_includes,
-  rects_have_common
-} from './geometry.js';
-import {
-  pre_render_pixel,
-  pre_render_square,
-  put_point,
-  put_point_1px,
-  draw_line_w_plot_func,
-  draw_line,
-  draw_line_w_runtime_renderer,
   draw_line_1px,
-  draw_rect,
-  draw_rect_R,
-  draw_circle,
-  FloodFillState,
-  get_max_run_len_histogram,
-  get_halftone_definition,
-  gen_halftones
+  FloodFillState
 } from './graphics.js';
 import {
-  getBrowserType,
-  unify_rect,
-  getBoundingClientRectWrp,
-  conv_page_client_to_wnd_client,
-  get_color_as_RGB,
-  get_color_as_RGBA,
-  get_components_from_RGBx,
-  get_cursor_color,
-  add_to_unique_list,
-  remove_from_unique_list,
-  PointManager,
-  register_pointer_event_handler,
-  change_selection,
-  ThicknessSelector,
-  SpKey,
-  KeyStateManager,
-  draw_icon_face,
-  borderColor,
-  activeIconColors,
-  inactiveIconColors,
-  textColor,
-  textCharWidth,
-  draw_icon_face_ex,
-  draw_icon_face_wrp,
-  draw_icon_ex,
-  draw_icon_wrp,
-  draw_color_palette,
-  MicroSlideBar,
-  ListBox
-} from './ui-util.js';
+  copy_layer, erase_single_layer,
+  fix_image_w_mask, get_destinaton_out_image,
+  get_mask_image
+} from './imaging.js';
 import {
-  ImagePatch,
-  DrawerBase,
-  NullDrawOp,
-  NullEffect,
-  NullCursor,
-  DrawOp_FreeHand,
+  ToolType
+} from './tool-palette.js';
+import {
+  Cursor_Circle,
+  Cursor_Square, DrawerBase,
+  DrawOp_BoundingRect, DrawOp_FreeHand,
   DrawOp_Rectangle,
   DrawOp_RectCapture,
-  DrawOp_BoundingRect,
-  EffectBase01,
-  Effect_Pencil,
   Effect_Eraser,
+  Effect_FlipRect, Effect_Pencil,
   Effect_PencilRect,
-  Effect_RectPaste,
-  Effect_RectEraser,
-  Effect_FlipRect,
-  Effect_Halftone,
-  CursorBase01,
-  Cursor_Circle,
-  Cursor_Square,
-  History,
-  UndoButton,
-  RedoButton
+  Effect_RectEraser, Effect_RectPaste, NullCursor
 } from './ui-element.js';
 import {
-  PointingEvent,
-  PointingEventClone,
-  VirtualClickEvent,
-  modify_click_event_to_end_in_place,
-  PictureCanvas
-} from './picture-canvas.js';
-// import {
-// 	DrawToolBase,
-// 	PencilTool,
-// 	FillRectTool,
-// 	LineRectTool,
-// 	CopyTool,
-// 	MirrorTool,
-// 	VertFlipTool,
-// 	EraseTool,
-// 	EraseRectTool,
-// 	ThicknessTool,
-// 	ColorPalette,
-// 	ColorCompoTool,
-// 	MaskTool,
-// 	get_layer_no,
-// 	PaintTool,
-// 	LayerTool,
-//  generateTool
-// } from './oebi-tool.js';
-import {
-  ToolType,
-  CommonSetting,
-  ToolChain,
-  addToolHelper,
-  ToolPalette
-} from './tool-palette.js';
-
-import {
-  g_pointManager,
-  g_keyStateManager,
-  g_pictureCanvas,
-  g_toolPalette,
-  g_paintTool,
-  g_history,
-  g_UndoButton
-} from './index.js';
+  draw_color_palette, draw_icon_ex,
+  draw_icon_wrp, get_color_as_RGB,
+  get_components_from_RGBx,
+  ListBox, MicroSlideBar, SpKey,
+  textColor
+} from './ui-util.js';
 
 'use strict';
 
